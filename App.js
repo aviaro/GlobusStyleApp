475d0027ddd6f7db4bd1bece909b879e15f91b37
aviaro <94477208+aviaro@users.noreply.github.com>
2021-11-17 11:19:46 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [height, setheight] =useState(0);
+  const [weight, setweight] = useState(0);
+  const [result, setresult] = useState(0);
+  const calbmi =() =>{
+    const hightformatter = height/100;
+    const bmi = weight /(hightformatter*hightformatter); ;
+    bmi = bmi.toFixed(2)
+    setResult(bmi)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>hello git</Text>
-      <StatusBar style="auto" />
+      <Text>please enter your heightin cm</Text>
+      <TextInput style={styles.inputstyle}
+      value={height}
+      onChangeText={text => setheight(text)}
+      />
+      <Text>PLEASE enter your weight</Text>
+      <TextInput style={styles.inputstyle}
+      value={weight}
+      onChangeText={text => setweight(text)}
+        
+      />
+      <Text>{height}</Text>
+     <TouchableOpacity onPress={calbmi} style={styles.calbutton}>calculate</TouchableOpacity>
+     <text>{result}</text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  inputstyle:{
+    backgroundColor:'#0ED0F9',
+    width:'100%',
+    borderRadius:20,
+    paddingHorizontal:10,
+    paddingVertical: 10,
+    margintop:6,
+    fontsize:16,
+    textalign:'center'
+  },
+  calbutton:{
+    fontsize:26,
+    backgroundColor: 'yellow'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding:'30%'
   },
 });
